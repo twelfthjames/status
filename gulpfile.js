4ba319e1a46e1b94263b9c4e5d06a438d533ebab
joelshaw <jshaw@rvadesignsolutions.com>
2018-12-02 17:12:46 -0500

    Added handlebars, htmlmin, updated browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ const gulp = require('gulp'),
       browserSync = require('browser-sync').create(),
       concat = require('gulp-concat'),
       eslint = require('gulp-eslint'),
+      handlebars = require('gulp-handlebars'),
+      htmlmin = require('gulp-htmlmin'),
       sass = require('gulp-sass');
 
 const src = './src',
       dist = './dist';
 
-gulp.task('serve', ['sass'], function(){
+gulp.task('serve', ['sass', 'minify', 'templates'], function(){
     browserSync.init({
         server: {
             baseDir: "./dist"
@@ -15,6 +17,7 @@ gulp.task('serve', ['sass'], function(){
     });
 
     gulp.watch(src + '/styles/**/*.scss', ['sass'])
+    gulp.watch(src + '/templates/*.hbs', ['templates'])
 });
 
 gulp.task('sass', function(){
@@ -24,8 +27,21 @@ gulp.task('sass', function(){
         .pipe(browserSync.stream());
 });
 
+gulp.task('minify', function(){
+    return gulp.src(src + '/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(dist));
+});
+
 gulp.task('scripts', function(){
     
+});
+
+gulp.task('templates', function(){
+    gulp.src(src + '/templates/*.hbs')
+        .pipe(handlebars())
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest(dist + '/js'));
 });
 
 gulp.task('default', ['serve']);
